f8c91182dd51a329f496d961274f0d742729276d
wilsenock220 <wilsenock220@gmail.com>
2019-08-14 08:42:28 +0300

    add initial commit

--- Neighbours/settings.py
+++ Neighbours/settings.py
@@ -20,7 +20,6 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -34,12 +33,10 @@ Moved to evn
 
 ALLOWED_HOSTS = []
 
-
 # login redirect
 
 LOGIN_REDIRECT_URL = '/'
 REGISTRATION_OPEN = True
-
 
 # Application definition
 
@@ -86,51 +83,48 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Neighbours.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 # if config("MODE")=='dev':
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config("DB_NAME"),
-        "USER": config("DB_USER"),
-        'PASSWORD': config("DB_PASSWORD"),
-        "HOST": config("DB_HOST")
-    }
-}
-# else:
-#     DATABASES={
-#     'default':dj_database_url.config(
-#     default=config("DATABASE_URL")
-#     )
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config("DB_NAME"),
+#         "USER": config("DB_USER"),
+#         'PASSWORD': config("DB_PASSWORD"),
+#         "HOST": config("DB_HOST")
 #     }
+# }
+# else:
+DATABASES = {'default': dj_database_url.config(default=config("DATABASE_URL"))}
 
-# db_from_evn=dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_evn)
+db_from_evn = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_evn)
 
-# ALLOWED_HOSTS=config("ALLOWED_HOSTS",cast=Csv)
-
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -145,17 +139,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
